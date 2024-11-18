dcd8fa0f1cc2d5565a2bb0632ad507b090dcdc30
Jan Galek <admin@gcore.cz>
2024-11-18 16:41:43 +0100

    Added dates

---
+++ datetime.go
@@ -0,0 +1,40 @@
+package datetime
+
+import (
+	"github.com/Gouef/utils"
+	"time"
+)
+
+type Date struct {
+	Year     int
+	Month    int `validate:"min=1,max=12"`
+	Day      int `validate:"min=1,max=31"`
+	DateTime time.Time
+}
+
+func NewDate(year int, month int, day int) *Date {
+	return &Date{Year: year, Month: month, Day: day, DateTime: GetDate(year, month, day)}
+}
+
+func GetDate(year int, month int, day int) time.Time {
+	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+}
+
+func (d *Date) IsWeekend() bool {
+	weekendDays := []time.Weekday{time.Sunday, time.Saturday}
+	return utils.InArray(d.DateTime.Weekday(), weekendDays)
+}
+
+func (d *Date) Time() time.Time {
+	return time.Date(d.Year, time.Month(d.Month), d.Day, 0, 0, 0, 0, time.UTC)
+}
+
+// Compare compares the date instant d with u. If d is before u, it returns -1;
+// if d is after u, it returns +1; if they're the same, it returns 0.
+func (d Date) Compare(u Date) int {
+	return d.Time().Compare(u.Time())
+}
+
+func (d Date) Equal(u Date) bool {
+	return d.Time().Equal(u.Time())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Gouef/datetime
 
 go 1.23.2
+
+require github.com/Gouef/utils v0.9.0
